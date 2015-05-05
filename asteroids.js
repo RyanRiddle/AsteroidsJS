b62200322693da76e70442e8e137ce26fcf3c36c
riddling <rcri222@gmail.com>
2015-05-05 01:19:32 -0400

    Separate velocity and direction
    Separated the angular velocity from the angle of direction.  Also allow
    the user to go as fast as they want.

--- asteroids.js
+++ asteroids.js
@@ -64,21 +64,22 @@ function Asteroid(anX, anY, aR, aTheta, aVelocity){
 function RocketShip(){
 	this.position = new Coordinate(500,500);
 	this.velocity = 0.0;
+	this.velAng = 0.0;
 	this.acceleration=0.0;
 	this.theta = 0.0;
 	this.leftWing = new Coordinate(this.position.x+(50*Math.cos(this.theta+(-1*Math.PI/6.0))), this.position.y+(50*Math.sin(this.theta+(-1*Math.PI/6.0))));
 	this.rightWing = new Coordinate(this.position.x+(50*Math.cos(this.theta+Math.PI/6.0)), this.position.y+(50*Math.sin(this.theta+Math.PI/6.0)));
 	this.update = update;
 	function update() {
-		if( this.velocity < 1.0 && this.velocity > -1.0 ){
-			this.velocity += this.acceleration;
-		}
-		else if( this.velocity >= 1.0 ){
-			this.velocity = 0.98;
-		}
-		else if( this.velocity <= -1.0 ){
-			this.velocity = -0.98;
-		}
+		var vx = this.velocity * Math.cos(this.velAng);
+		var vy = this.velocity * Math.sin(this.velAng);
+		var ax = this.acceleration * Math.cos(this.theta);
+		var ay = this.acceleration * Math.sin(this.theta);
+		vx += ax;
+		vy += ay;
+		this.velocity = Math.sqrt( Math.pow(vx, 2) + Math.pow(vy, 2) );
+		this.velAng = Math.atan2(vy,vx);
+			
 		if( this.position.x < 0 ){
 			this.position.x = canvas.width;
 		}
@@ -92,8 +93,8 @@ function RocketShip(){
 			this.position.y = 0;
 		}
 			
-		this.position.x += Math.cos(this.theta)*this.velocity;
-		this.position.y += Math.sin(this.theta)*this.velocity;
+		this.position.x += Math.cos(this.velAng)*this.velocity;
+		this.position.y += Math.sin(this.velAng)*this.velocity;
 		this.leftWing = new Coordinate(this.position.x+(50*Math.cos(this.theta+(-1*Math.PI/6.0))), this.position.y+(50*Math.sin(this.theta+(-1*Math.PI/6.0))));
 		this.rightWing = new Coordinate(this.position.x+(50*Math.cos(this.theta+Math.PI/6.0)), this.position.y+(50*Math.sin(this.theta+Math.PI/6.0)));
 	}
@@ -154,11 +155,14 @@ drawRocket = function(){
 	context.clearRect(0,0,canvas.width, canvas.height);
 	context.fillStyle = 'white';
 	var posString = "(" + rocket.position.x + ", " + rocket.position.y + ")";
-	var velString = rocket.velocity;
-	var accelString = rocket.acceleration;
+	var velString = rocket.velocity + " " + rocket.velAng;
+	var accelString = rocket.acceleration + " " + rocket.theta;
 	context.font="20px Arial";
 	context.fillText( "Lives: " + game.lives, 100, 20 );
 	context.fillText( "Points: " + game.points, 300, 20 );
+	//context.fillText(posString, 100, 100);
+	//context.fillText(velString, 100, 200);
+	//context.fillText(accelString, 100, 300);
 	rocket.update();
 	context.beginPath();
 	context.moveTo(rocket.leftWing.getX(), rocket.leftWing.getY());
@@ -237,7 +241,6 @@ rocketShipAsteroidCollision = function(rocketShip, asteroid)
 		var rightHalf = new Asteroid(asteroid.x, asteroid.y, asteroid.radius/2, Math.random()*Math.PI, asteroid.velocity);
 		asteroids.push(leftHalf);
 		asteroids.push(rightHalf);
-		//delete asteroid.x;
 	}
 	game.lives--;
 	game.collisionDetectionOn = false;
